26aa9d7121a6b50c54a4335113e14690bdf6dc4a
Gabriel Batista <batistagabriel360@gmail.com>
2019-11-20 15:30:38 -0300

    Adicionando url para tela inicial

--- website/website/urls.py
+++ website/website/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('index/', include('WebBsi.urls')),
 ]
