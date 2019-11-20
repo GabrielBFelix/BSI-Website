28b6bdb642dfe6e80fd5e337f092845164378132
Gabriel Batista <batistagabriel360@gmail.com>
2019-11-20 16:31:00 -0300

    Consertando erros nos modelos

--- website/WebBsi/models.py
+++ website/WebBsi/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 class Professor(models.Model):
     nome = models.CharField(max_length=40)
-    matricula = models.IntegerField(max_length=4)
+    matricula = models.IntegerField(4)
     username = models.CharField(max_length=40)
     senha = models.CharField(max_length=20)
     email = models.CharField(max_length=60)
 
 class Evento(models.Model):
-    codigo = models.IntegerField(max_length=4)
+    codigo = models.IntegerField(4)
     nome = models.CharField(max_length=40)
-    numeroVagas = models.IntegerField(max_length=4)
+    numeroVagas = models.IntegerField(4)
     local = models.CharField(max_length=40)
 
