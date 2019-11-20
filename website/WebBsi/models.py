66b666473debbbe8624ded2eb6a8a24a4d8dda29
Gabriel Batista <batistagabriel360@gmail.com>
2019-11-20 16:25:23 -0300

    Criando o banco de dados base

--- website/WebBsi/models.py
+++ website/WebBsi/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Professor(models.Model):
+    nome = models.CharField(max_length=40)
+    matricula = models.IntegerField(max_length=4)
+    username = models.CharField(max_length=40)
+    senha = models.CharField(max_length=20)
+    email = models.CharField(max_length=60)
+
+class Evento(models.Model):
+    codigo = models.IntegerField(max_length=4)
+    nome = models.CharField(max_length=40)
+    numeroVagas = models.IntegerField(max_length=4)
+    local = models.CharField(max_length=40)
+
