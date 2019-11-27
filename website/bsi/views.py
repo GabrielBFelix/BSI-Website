0332dc60502bd6e9d48cce3c0f7a62d621120f7c
Gabriel Batista <batistagabriel360@gmail.com>
2019-11-27 14:48:31 -0300

    Adicionado as telas de professor, aluno, laboratorio, noticia, projeto

--- website/bsi/views.py
+++ website/bsi/views.py
@@ -4,17 +4,17 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("HOME PAGE")
 
-def professor(request, id, nome):
-    return HttpResponse("Olhando o professor com id %s e com o nome %s." % id % nome)
+def professor(request, id):
+    return HttpResponse("Olhando o professor com id %i" % id)
 
-def Aluno(request, id, nome):
-    return HttpResponse("Olhando o Aluno com id %s e com o nome %s." % id % nome)
+def aluno(request, id):
+    return HttpResponse("Olhando o Aluno com id %i" % id)
 
-def Laboratorio(request, id, nome):
-    return HttpResponse("Olhando o Laboratorio com id %s e com o nome %s." % id % nome)
+def laboratorio(request, id):
+    return HttpResponse("Olhando o Laboratorio com id %i" % id)
 
-def Noticia(request, id, nome):
-    return HttpResponse("Olhando o Noticia com id %s e com o nome %s." % id % nome)
+def noticia(request, id):
+    return HttpResponse("Olhando o Noticia com id %i" % id)
 
-def Projeto(request, id, nome):
-    return HttpResponse("Olhando o Projeto com id %s e com o nome %s." % id % nome)
+def projeto(request, id):
+    return HttpResponse("Olhando o Projeto com id %i" % id)
