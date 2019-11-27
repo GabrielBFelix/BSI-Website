81118a9fed85548236ba73cde1f96cebd2ea61a4
Gabriel Batista <batistagabriel360@gmail.com>
2019-11-27 14:03:33 -0300

    adicionando umas views aq

--- website/bsi/views.py
+++ website/bsi/views.py
@@ -3,3 +3,18 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("HOME PAGE")
+
+def professor(request, id, nome):
+    return HttpResponse("Olhando o professor com id %s e com o nome %s." % id % nome)
+
+def Aluno(request, id, nome):
+    return HttpResponse("Olhando o Aluno com id %s e com o nome %s." % id % nome)
+
+def Laboratorio(request, id, nome):
+    return HttpResponse("Olhando o Laboratorio com id %s e com o nome %s." % id % nome)
+
+def Noticia(request, id, nome):
+    return HttpResponse("Olhando o Noticia com id %s e com o nome %s." % id % nome)
+
+def Projeto(request, id, nome):
+    return HttpResponse("Olhando o Projeto com id %s e com o nome %s." % id % nome)
